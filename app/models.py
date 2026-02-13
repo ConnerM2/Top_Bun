9826bede5761a4bb7156bb3e211ba43ecbe10251
ConnerM Windows <manwillconner3@gmail.com>
2026-02-12 19:00:51 -0700

    Refactor store relationship and add store-related routes and templates

--- app/models.py
+++ app/models.py
@@ -10,7 +10,7 @@ class Store(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     location: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
 
-    assessments = db.relationship("Response", backref='Store')
+    evals = db.relationship("Response", backref='Store')
 
 class Assessment(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from app import app, models
+from app import app
+from app.models import Store, Assessment, Question, Response, Answer
 from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
 
@@ -13,3 +14,19 @@ def login():
         flash('Logged in!')
         return redirect(url_for('index'))
     return render_template('login.html', title="Login", form=form)
+
+@app.route('/stores')
+def stores():
+    stores = Store.query.all()
+    return render_template("stores_dashboard.html", stores=stores)
+
+@app.route('/stores/<int:store_id>')
+def store_page(store_id):
+    store = Store.query.get_or_404(store_id)
+    return render_template("store_page.html", store=store)
+
+@app.route('/stores/<int:store_id>/<int:assessment_id>')
+def assessment_page(store_id, assessment_id):
+    store = Store.query.get_or_404(store_id)
+    assessment = Assessment.query.get_or_404(assessment_id)
+    return render_template("assessment.html", assessment=assessment)
