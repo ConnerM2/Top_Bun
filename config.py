5d333d500db1a69f86e65b01ab2d1e2eca4b78a3
ConnerM Windows <manwillconner3@gmail.com>
2026-03-04 12:16:42 -0700

    Update config.py to provide default values for SECRET_KEY and SQLALCHEMY_DATABASE_URI

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
